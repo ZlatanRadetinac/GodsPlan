9e914712884a53c48e7829ab3624c482af36d0a8
Andrija Jovanović <ja.andrija@outlook.com>
2019-10-05 14:51:19 +0200

    things that probably don't work

--- GodsPlan/Assets/Scripts/Freestyle/InsultProvider.cs
+++ GodsPlan/Assets/Scripts/Freestyle/InsultProvider.cs
@@ -1,8 +1,8 @@
-﻿using UnityEngine;
-using UnityEditor;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Collections.Generic;
 using System.Linq;
+using UnityEditor;
+using UnityEngine;
 
 public class InsultProvider : ScriptableObject
 {
@@ -21,14 +21,18 @@ public class InsultProvider : ScriptableObject
         }
     }
 
+    public InsultProvider(TextAsset textAsset)
+    {
+
+    }
+
+    public void LoadInsults()
+    {
+        AssetDatabase.LoadAssetAtPath("../../Resources/Text/insults.txt", typeof(TextAsset));
+    }
+
     public Insult GetRandomInsult()
     {
         return Insults.ElementAt(Random.Range(0, Insults.Count));
-    }
-
-    [MenuItem("Tools/MyTool/Do It in C#")]
-    static void DoIt()
-    {
-        EditorUtility.DisplayDialog("MyTool", "Do It in C# !", "OK", "");
     }
 }
