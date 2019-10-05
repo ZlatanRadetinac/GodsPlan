0f1e9e0a57c0b7bbe7736ecbc81a3b8a44820088
Andrija Jovanović <ja.andrija@outlook.com>
2019-10-05 13:41:55 +0200

    InsultProvider.cs

--- GodsPlan/Assets/Scripts/Freestyle/Insult.cs
+++ GodsPlan/Assets/Scripts/Freestyle/Insult.cs
@@ -1,18 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 public class Insult : MonoBehaviour
 {
+    public Insult(string challenge, string response)
+    {
+        ChallengeLine = challenge;
+        CorrectResponseLine = response;
+    }
+
+    public string ChallengeLine { get; }
+
+    public string CorrectResponseLine { get; }
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    public bool ValidateResponse(string responseLine)
+    {
+        return (responseLine.Equals(CorrectResponseLine, StringComparison.InvariantCultureIgnoreCase));
     }
 }
---
+++ GodsPlan/Assets/Scripts/Freestyle/InsultProvider.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+public class InsultProvider : ScriptableObject
+{
+    private HashSet<Insult> Insults { get; }
+
+    public InsultProvider(StreamReader reader)
+    {
+        Insults = new HashSet<Insult>();
+
+        while (!reader.EndOfStream)
+        {
+            var challenge = reader.ReadLine();
+            var response = reader.ReadLine();
+
+            Insults.Add(new Insult(challenge, response));
+        }
+    }
+
+    public Insult GetRandomInsult()
+    {
+        return Insults.ElementAt(Random.Range(0, Insults.Count));
+    }
+
+    [MenuItem("Tools/MyTool/Do It in C#")]
+    static void DoIt()
+    {
+        EditorUtility.DisplayDialog("MyTool", "Do It in C# !", "OK", "");
+    }
+}
